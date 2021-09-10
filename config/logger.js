ce035ec43b54892c26b1d9130e8dd310ccf33708
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2021-09-10 18:24:24 +0300

    Small fix for correct timestamps in logs

--- config/logger.js
+++ config/logger.js
@@ -5,18 +5,18 @@ const moment = require('moment');
 const usersLogPath = path.join(__dirname, '../log/users.log');
 const errorLogPath = path.join(__dirname, '../log/error.log');
 
-const timestamp = moment().format('HH:mm:ss DD.MM.YYYY');
-
 const addEntryToUserLog = (update) => {
+  const timestamp = moment().format('HH:mm:ss DD.MM.YYYY');
   const { message } = update;
   const user = message.from;
-  const userInfo = `ID ${user.id} | Name: ${user.first_name} ${user.last_name}| Username @${user.username} | ${user.language_code}`;
+  const userInfo = `ID: ${user.id} | Name: ${user.first_name} ${user.last_name}| Username: @${user.username} | LC: ${user.language_code}`;
   const string = `${timestamp.toString()} ${userInfo}\n`;
 
   fs.appendFileSync(usersLogPath, string);
 };
 
 const addEntryToErrorLog = (error) => {
+  const timestamp = moment().format('HH:mm:ss DD.MM.YYYY');
   const string = `${timestamp.toString()} ${error}\n`;
 
   fs.appendFileSync(errorLogPath, string);
